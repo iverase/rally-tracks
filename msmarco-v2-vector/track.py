48dde38b59bf67f021238a9964dc9615b2afdb6d
ivera <ignacio.vera@elastic.co>
2026-07-06 09:38:47 +0100

    slices-rally

--- msmarco-v2-vector/track.py
+++ msmarco-v2-vector/track.py
@@ -38,7 +38,7 @@ class BulkSliceParamSource:
 
     def __init__(self, track, params, **kwargs):
         self._inner = BulkIndexParamSource(track, params, **kwargs)
-         self.corpora = self._inner.corpora
+        self.corpora = self._inner.corpora
         self.infinite = self._inner.infinite
         self._seed = params.get("slice-random-seed", 42)
 
